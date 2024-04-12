c06e9f6ab80f08e9b53397c787953f80b694358d
Daniel Dahme <dahme@qualitus.de>
2024-04-12 09:52:42 +0200

    module navi icons added

--- tpl/Page/ModuleNavi.php
+++ tpl/Page/ModuleNavi.php
@@ -1,8 +1,12 @@
 <ul>
 <?php
 	foreach ($this->_['modules'] as $module) {
+		// TODO remove old way
+		$icon = isset($module['icon'])
+			? $module['icon']
+			: 'modules/' . $module['module'] . '/icon.png';
 ?>
-	<li><a href="#" rel="<?php echo $module['module']; ?>"><img border="0" src="modules/<?php echo $module['module']; ?>/icon.png" /><?php echo $module['name']; ?></a></li>
+	<li><a href="#" rel="<?php echo $module['module']; ?>"><img border="0" src="<?php echo $icon; ?>" /><?php echo $module['name']; ?></a></li>
 <?php
 	}
 ?>
